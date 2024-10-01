3dd1dbdb280ba3884404f05a3479a1932cb01595
Romit  Dey <romit77dey@gmail.com>
2024-10-01 12:56:16 +0530

    ci check

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -36,14 +36,13 @@ export const AuroraHero = () => {
     >
       <div className="relative z-10 flex flex-col items-center">
         <span className="mb-1.5 inline-block rounded-full bg-gray-600/50 px-3 py-1.5 text-sm">
-          Beta Now Live!
+          Now Live!
         </span>
         <h1 className="max-w-3xl bg-gradient-to-br from-white to-gray-400 bg-clip-text text-center text-3xl font-medium leading-tight text-transparent sm:text-5xl sm:leading-tight md:text-7xl md:leading-tight">
           Decrease your SaaS churn by over 90%
         </h1>
         <p className="my-6 max-w-xl text-center text-base leading-relaxed md:text-lg md:leading-relaxed">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae, et,
-          distinctio eum impedit nihil ipsum modi.
+          Decrease your SaaS churn by over 90% with our innovative solutions
         </p>
         <motion.button
           style={{
