9ed0665d371f14a1e37ade4aedbdbf3a5c154655
Aditya Gupta <adisamarth20@gmail.com>
2025-10-31 22:10:45 +0530

    feat: add todo component

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -7,7 +7,7 @@ import React from 'react';
 export default function Fake() {
   return (
     <div className="fake-component" style={{padding: '1rem', border: '1px dashed #999'}}>
-      <h3>Fake.jsxx Component</h3>
+      <h3>Todo.jsxx Component</h3>
       <p>This is a fake component file with a nonstandard <code>.jsxx</code> extension.</p>
     </div>
   );
